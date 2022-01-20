222f7a25d00006001a876362b8bec36a63a2c357
tim <timdebooij9@hotmail.com>
2022-01-20 23:29:47 +0100

    [Added] virtenv

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+activate_this = '/path/to/env/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
 import flask
 from flask import Flask
 from flask import request, jsonify
